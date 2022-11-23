76cd48ccab780cd391fa05a4de59a3dd576d3578
Mughees Butt <mughees.butt@devsinc.com>
2022-11-24 01:50:41 +0500

    Addition of quote model

---
+++ db/migrate/20221123200803_create_quotes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# db/migrate/XXXXXXXXXXXXXX_create_quotes.rb
+
+class CreateQuotes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :quotes do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
