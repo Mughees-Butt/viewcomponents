d6c546d64b8023f54ca366ae7ead30dd88bd831e
Mughees Butt <mughees.butt@devsinc.com>
2022-11-24 01:55:03 +0500

    Addition of quote controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# config/routes.rb
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+Rails.application.routes.draw do
+  root to: 'quotes#index'
+  resources :quotes
 end
